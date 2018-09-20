d46e1cc9fb68eb4c16b30b3878d74c912e1b176b
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-09-20 16:12:10 -0400

    added exercice 2

--- exercices/template-p5-project/js/script.js
+++ exercices/template-p5-project/js/script.js
@@ -13,23 +13,24 @@ author, and this description to match your project!
 // Description of preload
 
 function preload() {
-
 }
 
 
-// setup()
-//
-// Description of setup
+var typedText = "";
 
 function setup() {
-
+  createCanvas(500,500);
+  textSize(24);
+  textAlign(CENTER,CENTER);
 }
 
-
-// draw()
-//
-// Description of draw()
-
 function draw() {
+  if (keyIsPressed) {
+    background (255,255,255,30)
+  }
+  text(typedText,width/2,height/2);
+}
 
+function keyTyped() {
+  typedText += key;
 }
