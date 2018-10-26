17cadf5933c4fa10eae38127f1bbfdcd0a2f9238
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-10-25 23:35:25 -0400

    E6: missing } on line 30

--- exercices/exercise6/js/script.js
+++ exercices/exercise6/js/script.js
@@ -26,32 +26,33 @@ function setup() {
   rightPaddle = new Paddle(width-10,height/2,10,600,10,UP_ARROW,DOWN_ARROW);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(0,height/2,10,60,10,83,87);
-  }
+  leftPaddle = new Paddle(0,height/2,10,60,10,83,87;
+    //////////////// FIXED
+}
 
 
-  // draw()
-  //
-  // Handles input, updates all the elements, checks for collisions
-  // and displays everything.
-  function draw() {
-    background(0);
+    // draw()
+    //
+    // Handles input, updates all the elements, checks for collisions
+    // and displays everything.
+    function draw() {
+      background(0);
 
-    leftPaddle.handleInput();
-    rightPaddle.handleInput();
+      leftPaddle.handleInput();
+      rightPaddle.handleInput();
 
-    ball.update;
-    leftPaddle.update();
-    rightPaddle.update();
+      ball.update;
+      leftPaddle.update();
+      rightPaddle.update();
 
-    if (ball.isOffTheScreen())
-    reset();
-  }
+      if (ball.isOffTheScreen())
+      reset();
+    }
 
-  ball.handleCollision(leftPaddle);
-  ball.handleCollision(rightPaddle);
+    ball.handleCollision(leftPaddle);
+    ball.handleCollision(rightPaddle);
 
-  ball.display();
-  leftPaddle.display();
-  rightPaddle.display(;
-  }
+    ball.display();
+    leftPaddle.display();
+    rightPaddle.display(;
+    }
