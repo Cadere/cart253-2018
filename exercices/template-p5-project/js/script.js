c915a281c18f43936d912a54a8d5b7fdc69551d9
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-01 16:22:34 -0400

    X: Reset default template file

--- exercices/template-p5-project/js/script.js
+++ exercices/template-p5-project/js/script.js
@@ -8,20 +8,13 @@ author, and this description to match your project!
 
 ******************/
 
-var x;
-var y;
-var tx;
-var ty;
+
+
 function setup() {
-  createCanvas(500,500);
-  tx = random(0,1000);
-  ty = random(0,1000);
+
+
 }
+
 function draw() {
-  background(255);
-  x = width * noise(tx);
-  y = height * noise(ty);
-  tx += 0.01;
-  ty += 0.01;
-  ellipse(x,y,10,10);
+
 }
