a17421510e22a4a1c8d6fc5f266ba0ec5c28a7c7
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-02 12:49:53 -0400

    P2: Added scoreboard constructor

---
+++ projects/project2/js/Scoreboard.js
@@ -0,0 +1,16 @@
+// Scoreboard
+//
+// A class that defines how a scoreboard acts
+// Includes displaying the player score
+
+// Scoreboard constructor
+//
+// Sets the properties with the provided arguments
+function Scoreboard (x,y,width,height,fill,name){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.fill = fill;
+  this.name = name;
+}
