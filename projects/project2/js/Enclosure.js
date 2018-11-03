5179230f6fa4f10bcafd7bac4ba029e6edeb848a
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-03 12:30:06 -0400

    P2: Begun new object: Enclosure

---
+++ projects/project2/js/Enclosure.js
@@ -0,0 +1,14 @@
+//Enclosure
+//
+//A class that defines how an enclosure acts
+//the enclosure handles collisions with the ball and resets it
+//but it does not dock points to the players
+//
+//Enclosure constructor
+function Enclosure(x,y,width,height,fill){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.fill = fill;
+}
