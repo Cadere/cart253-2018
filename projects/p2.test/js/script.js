0a4ff8a7f9927d4cabb2f2722f781e48b616a180
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-03 15:59:58 -0400

    P2: created a P2 test script to test entirely new stuff without messing with my code. Started building an interactive menu

--- projects/p2.test/js/script.js
+++ projects/p2.test/js/script.js
@@ -7,15 +7,17 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-
+var stateArray;
+var menu;
 
 
 function setup() {
-
+createCanvas(640,480);
 
 }
 
 function draw() {
+  background(0);
   // Set up all the styling elements
   push();
   textAlign(CENTER,CENTER);
@@ -24,11 +26,11 @@ function draw() {
   fill(255);
   stroke(255);
   // Display the text
-  text("HERDER PONG",width/2,height/2);
+  text("HERDER PONG",width/2,height/5);
   // Font size goes down
   textSize(16);
   // Display the instructions
-  text("Press SPACE to play\nUse WS and UP AND DOWN ARROWS",width/2,3*height/4);
+  text("Press SPACE to play\nUse WS and UP AND DOWN ARROWS",width/2,height*0.9);
   pop();
 
 }
