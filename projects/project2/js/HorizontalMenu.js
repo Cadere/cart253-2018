dd98773261d47e3fdd5dcdc0bc2eaef7897c10de
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-05 14:14:30 -0500

    p2: started working on the character select menu

---
+++ projects/project2/js/HorizontalMenu.js
@@ -0,0 +1,67 @@
+//HorizontalMenu
+//
+//This is a class for building a menu in which you scroll with arrows from side to side
+//
+//Menu constructor
+function HorizontalMenu(centerHeight,choiceNumber,leftKey,rightKey,confirmKey,menuState,commands){
+  this.centerHeight = centerHeight;
+  //this is the number of choices in the menu
+  this.choiceNumber = choiceNumber;
+  //this is the menu's distance from the edge of the canvas
+  this.edge = width/5;
+  this.size = width*.6;
+  // this is the state of the menu i.e. what is selected
+  this.state = 0;
+  //these are the keys to move in the menu
+  this.leftKey = leftKey;
+  this.rightKey = rightKey;
+  //this is the key to confirm a choice
+  this.confirmKey = confirmKey;
+  //this is the commands text
+  this.commands = commands;
+}
+
+//display()
+//
+//this method displays the menu
+HorizontalMenu.prototype.display = function(){
+  push();
+  //this diplsyas a rectangle under the selected option
+  rectMode(CENTER);
+  fill(150);
+  //the rectangle moves because it uses information in stateArray to define its position
+  rect(this.menuState[this.state].y, this.centerHeight, this.size/this.choiceNumber, 100);
+  //this displays the title text
+  textAlign(CENTER,CENTER);
+  textFont("Agency FB");
+  textSize(24);
+  fill(255);
+  text(this.commands,width/2, this.centerHeight+100);
+  pop();
+}
+
+
+
+// this method handles keyboard imput in order to change the menu's state
+HorizontalMenu.prototype.handleInput= function(){
+ if (keyIsDown(this.upKey)){
+   //if the state of the menu is at the maximum, it resets to state 1
+   //otherwise the state of the menu goes up by one
+   if(this.state === this.choiceNumber){
+     this.state = 1;
+   }
+   else{
+     this.state += 1;
+   }
+ }
+ //if the state of the menu is at 0 or 1, it goes to state 3
+ //it is impossible to go back to state 0 once UP_ARROW or DOWN_ARROW has been pressed
+ else if (keyIsDown(this.downKey)){
+   if(this.state === 0 || this.state === 1){
+     this.state = this.choiceNumber;
+   }
+   else{
+     this.state -= 1;
+   }
+ }
+}
