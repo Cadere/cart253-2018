c83dd1b8bf1cb87f58954342164f108a98c32e33
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-15 14:16:26 -0500

    Added p5 sound library to template

--- exercices/template-p5-project/js/script.js
+++ exercices/template-p5-project/js/script.js
@@ -9,26 +9,11 @@ author, and this description to match your project!
 ******************/
 
 
-
 function setup() {
 
 
 }
 
 function draw() {
-  // Set up all the styling elements
-  push();
-  textAlign(CENTER,CENTER);
-  textFont("Agency FB");
-  textSize(32);
-  fill(255);
-  stroke(255);
-  // Display the text
-  text("HERDER PONG",width/2,height/2);
-  // Font size goes down
-  textSize(16);
-  // Display the instructions
-  text("Press SPACE to play\nUse WS and UP AND DOWN ARROWS",width/2,3*height/4);
-  pop();
 
 }
