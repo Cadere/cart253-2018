ed3ea7daf027f44d580c0a7a87aff1be350c4990
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-11-22 15:44:00 -0500

    E8: Replaced position pushes with loops

--- exercices/exercise8/js/script.js
+++ exercices/exercise8/js/script.js
@@ -11,15 +11,15 @@ it will be used to create a memory card game
 ******************/
 
 //variables for the card grid
-var column = 3;
+var column = 4;
 //rangee is the number of lines on the grid - had to resort to a french name since line is already a thing in p5
-var rangee = 2;
+var rangee = 3;
 var cardNb = rangee*column;
 //variable for the cards
 var card = [];
 //variables for the positions
-var pY;
-var pX;
+var pY = [];
+var pX = [];
 var position = [];
 var shuffledPosition;
 //variables for the images
@@ -41,22 +41,34 @@ function setup() {
   createCanvas(1000,800);
   //create positions
   //variables for the positions
-  console.log(rangee+1);
-  pY = [height/(rangee+1),height/(rangee+1)*2];
-  pX = [width/(column+1),width/(column+1)*2,width/(column+1)*3];
-  //in a very uneficient manner
-  position.push(new Position(pX[0],pY[0]));
-  position.push(new Position(pX[1],pY[0]));
-  position.push(new Position(pX[2],pY[0]));
-  position.push(new Position(pX[0],pY[1]));
-  position.push(new Position(pX[1],pY[1]));
-  position.push(new Position(pX[2],pY[1]));
+  //////// NEW /////////
+  // replaced manually setting pY and pX contents with loops
+  // so that the code adapts to changing the number of cards more easily
+  for (var i = 0; i < rangee; i++){
+    pY.push(height/(rangee+1)*(i+1));
+  }
+  for (var i = 0; i < column; i++){
+    pX.push(width/(column+1)*(i+1));
+  }
+
+  // Replaced manually pushing the positions with a loop
+  for (var i = 0; i < rangee; i++){
+    for (var j = 0; j < column; j++){
+      position.push(new Position(pX[j],pY[i]));
+    }
+  }
 
   //this shuffles the order of the positions
   shuffledPosition = shuffle(position);
   //this allocates positions to cards
   for (var i = 0; i < cardNb; i++){
-  card.push(new Card(cardFace[i],shuffledPosition[i]));
+    if(i < cardNb/2){
+      card.push(new Card(cardFace[i],shuffledPosition[i]));
+    }
+    //this allocates the same images to positions again so that each image appears twice
+    else{
+      card.push(new Card(cardFace[i-(cardNb/2)],shuffledPosition[i]));
+    }
   }
   console.log(card[0].position)
 }
