0417370ce994949fb66556aab23d2101dc472f78
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2018-12-06 13:05:23 -0500

    P3: Made new project folder to test the particle library I will be using

---
+++ projects/p3.test/js/script.js
@@ -0,0 +1,19 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+function setup() {
+
+
+}
+
+function draw() {
+
+}
